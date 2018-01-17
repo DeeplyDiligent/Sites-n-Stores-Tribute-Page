0f297084b798eea14f7ed9ced9f73a1f41dabdfc
Ko0lGuY <bha0011@jmss.vic.edu.au>
2018-01-17 19:22:09 +1100

    removed initial animation and added black line

--- animate.js
+++ animate.js
@@ -40,18 +40,19 @@ $(document).ready(function () {
 			// animate to second panel
 //            .staggerTo(".animateThis", 2, {scale:2},0.5)
 //            .staggerTo(".animateThis", 2, {scale:1},0.5,0)
-            .staggerTo("#panel-1 .animateThis", 0.1, {ease:Back.easeOut, x:100, cycle:{x:200},scale:1.5, opacity:0}, 0.03)
-			.to("#slideContainer", 0.5, {z: -150})		// move back in 3D space
+            .staggerTo("#panel-1 .animateThis", 0.1, {ease:SlowMo.easeOut, x:100, cycle:{x:200},scale:1.5, opacity:0}, 0.01,"titleout")
+            .to(".black-line",0.5,{ease:SlowMo.easeOut, y:"400"},"titleout")
+			.to("#slideContainer", 0.5, {z: -150},"titleout")		// move back in 3D space
 			.to("#slideContainer", 1,   {x: "-25%"})	// move in to first panel
-			.to("#slideContainer", 0.5, {z: 0})				// move back to origin in 3D space
+			.to("#slideContainer", 0.5, {z: 0},"load 2")				// move back to origin in 3D space
 			// animate to third panel
-            .staggerFrom("#panel-2 .animateThis", 0.1, {y:-200, opacity:0, scale:3}, 0.005)
+            .staggerFrom("#panel-2 .animateThis", 0.15, {y:-200, opacity:0, scale:3}, 0.01,"load 2")
 			.to("#slideContainer", 0.5, {z: -150})
 			.to("#slideContainer", 1,   {x: "-50%"})
 			.to("#slideContainer", 0.5, {z: 0})
             
 			// animate to forth panel
-            .staggerFrom("#panel-3 .animateThis", 0.1, {x:-200, opacity:0, scale:3}, 0.005)
+            .staggerFrom("#panel-3 .animateThis", 0.1, {x:-200, opacity:0, scale:4}, 0.005)
 			.to("#slideContainer", 0.5, {z: -150})
 			.to("#slideContainer", 1,   {x: "-75%"})
 			.to("#slideContainer", 0.5, {z: 0},"zoom-in-last")
